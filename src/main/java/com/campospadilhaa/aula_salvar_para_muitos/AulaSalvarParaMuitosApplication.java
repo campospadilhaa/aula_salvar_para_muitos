3914c807374baa1c41f6d1cb392be0b32048f7ec
Anderson Campos Padilha <campospadilha.a@gmail.com>
2025-02-26 08:59:50 -0300

    [200~Projeto contendo exemplo de gravação de entidade associada: Person com Department

---
+++ src/main/java/com/campospadilhaa/aula_salvar_para_muitos/AulaSalvarParaMuitosApplication.java
@@ -0,0 +1,13 @@
+package com.campospadilhaa.aula_salvar_para_muitos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaSalvarParaMuitosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaSalvarParaMuitosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campospadilhaa/aula_salvar_para_muitos/AulaSalvarParaMuitosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campospadilhaa.aula_salvar_para_muitos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaSalvarParaMuitosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
